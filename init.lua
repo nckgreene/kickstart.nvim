8a64ebfe3a9cdbb525fd99a1c83af5f401b41683
Nick Greene <ngreene2508@gmail.com>
2026-01-12 14:54:36 +1300

    Remove ensure_installed in treesitter

--- init.lua
+++ init.lua
@@ -1020,7 +1020,8 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
+      -- ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python' },
+      ensure_installed = {},
       -- Autoinstall languages that are not installed
       auto_install = true,
       sync_install = false,
